90aee02b0b798a99cede171ad6026aebf1d063c1
Mustafa Bilir <mustafabilir@live.com>
2017-06-26 00:26:44 +0300

    use heroku assigned port number

--- stream.py
+++ stream.py
@@ -1,8 +1,13 @@
+import os
 import streamlink 
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/<path:link>")
 def generateLink(link):
 	streams = streamlink.streams(link)
 	return streams['best'].url
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=os.environ['PORT'])
