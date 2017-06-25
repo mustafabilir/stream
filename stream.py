19ccdfd9e4b78849914512e3260f0407673ce932
Mustafa Bilir <mustafabilir@live.com>
2017-06-26 01:23:36 +0300

    allow origin access decorator

--- stream.py
+++ stream.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "http://www.mustafabilir.com"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/<path:link>")
 def generateLink(link):
